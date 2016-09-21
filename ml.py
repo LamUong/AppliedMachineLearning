378f5bd8e69d4d6061fc96e033d393ebfbce6cf6
Lam Uong <lam.uong2@mail.mcgill.ca>
2016-09-20 21:05:27 -0400

    update

--- ml.py
+++ ml.py
@@ -39,11 +39,11 @@ def summarizeddata(trainset,datasetcommand = ['Discrete','Discrete','Discrete','
 	
 	HashbyClass = {}
 	for data in trainset:
-		if data[-2] in HashbyClass:
-			HashbyClass[data[-2]].append(data)
+		if data[4] in HashbyClass:
+			HashbyClass[data[4]].append(data)
 		else:
-			HashbyClass[data[-2]] = []
-			HashbyClass[data[-2]].append(data)
+			HashbyClass[data[4]] = []
+			HashbyClass[data[4]].append(data)
 	
 	HashofClasses = {}
 	HashofClasses['ratio'] = {}
@@ -105,17 +105,19 @@ def predict(testset,summarizeddata,datasetcommand = ['Continuous','Discrete','Di
 			probY = summarizeddata['ratio'][Class]
 			probXgivenY = 1
 			for i in range(0,len(data),1):
+				
 				if datasetcommand[i] == 'ID' or datasetcommand[i] == 'Result':
 					pass
+
 				elif datasetcommand[i] == 'Discrete':
 					if i not in summarizeddata[Class]:
 						pass
 					else:
 						print 'shitme'
-
 						probXgivenY *= float(summarizeddata[Class][i][data[i]])/summarizeddata[Class][i]['totalcount']
+				
 				elif datasetcommand[i] == 'Continuous':
-					print data[i], summarizeddata[Class][i]['avg'], summarizeddata[Class][i]['stdev']
+					#print data[i], summarizeddata[Class][i]['avg'], summarizeddata[Class][i]['stdev']
 					#print calculateProbability(data[i], summarizeddata[Class][i]['avg'], summarizeddata[Class][i]['stdev'])
 					probXgivenY *= calculateProbability(data[i], summarizeddata[Class][i]['avg'], summarizeddata[Class][i]['stdev'])
 					
@@ -130,8 +132,7 @@ def predict(testset,summarizeddata,datasetcommand = ['Continuous','Discrete','Di
 			t+=1
 		else:
 			f+=1
-	print t 
-	print f
+	print float(t)/(f+t) 
 			
 
 a = load.loaddata()
@@ -139,9 +140,8 @@ a = load.loaddata()
 
 train,test = splitdata(a,0.67)
 
-summarizeddata = summarizeddata(train,datasetcommand = ['Continuous','Discrete','Discrete','Discrete','Result','ID'])
-
-predict(test,summarizeddata,datasetcommand = ['Continuous','Discrete','Discrete','Discrete','Result','ID'])
+summarizeddata = summarizeddata(train,datasetcommand = ['Continuous','Discrete','Discrete','Continuous','Result','ID'])
+predict(test,summarizeddata,datasetcommand = ['Continuous','Discrete','Discrete','Continuous','Result','ID'])
 
 '''
 data = loadCsv('data.csv')
--- scikit.py
+++ scikit.py
@@ -1,7 +1,29 @@
 from sklearn import datasets
 iris = datasets.load_iris()
 from sklearn.naive_bayes import GaussianNB
+import load
+
 gnb = GaussianNB()
-y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-print("Number of mislabeled points out of a total %d points : %d"
-       % (iris.data.shape[0],(iris.target != y_pred).sum()))
+
+wholedata = load.loaddata()
+
+x = []
+y = []
+for data in wholedata:
+	x.append(data[:-1])
+	y.append(data[-1])
+
+
+
+
+
+y_pred = gnb.fit(x, y).predict(x)
+a = 0
+b = 0
+for y1,y2 in zip(y_pred,y):
+	if y1==y2:
+		a+=1
+	else:
+		b+=1
+print a
+print b
